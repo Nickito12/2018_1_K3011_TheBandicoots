13d7f86fcd7ac214c20b0aaf272e4d302e56294c
MauroMowa <mowa_mauro95@hotmail.com>
2018-05-06 00:09:01 -0300

    escenario v10

--- TGC.Group/Model/GameObjects/Escenario1.cs
+++ TGC.Group/Model/GameObjects/Escenario1.cs
@@ -27,13 +27,13 @@ namespace TGC.Group.Model.GameObjects
             var PisoWidth = 1200f;
             var PisoLength = PisoWidth;
             var PisoTexture = TgcTexture.createTexture(D3DDevice.Instance.Device, Env.MediaDir + "pasto.jpg");
-            Piso = new TgcPlane(new TGCVector3(PisoWidth * -0.5f, 0f, PisoWidth * -0.5f), new TGCVector3(PisoWidth, 20f, PisoWidth), TgcPlane.Orientations.XZplane, PisoTexture, 15, 15);
+            Piso = new TgcPlane(new TGCVector3(/*PisoWidth * -0.5f*/-200, 0f, PisoWidth * -0.5f), new TGCVector3(PisoWidth, 20f, PisoWidth), TgcPlane.Orientations.XZplane, PisoTexture, 15, 15);
 
             //Crear SkyBox
             CreateSkyBox(TGCVector3.Empty, new TGCVector3(10000, 10000, 10000), "SkyBox1");
 
             Loader = new TgcSceneLoader();
-            Scene = Loader.loadSceneFromFile(Env.MediaDir + "\\" + "Escenario1\\ESCENACALABERITANUEVA-TgcScene.xml");
+            Scene = Loader.loadSceneFromFile(Env.MediaDir + "\\" + "Escenario1\\asd10-TgcScene.xml");
 
             mp3Player = new TgcMp3Player();
             mp3Player.FileName = Env.MediaDir + "\\Sound\\song.mp3";
