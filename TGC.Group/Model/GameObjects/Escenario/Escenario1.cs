00bee0722c94721ba6598143eeb5b9dc55a6184b
Nicolas <abramlujan@gmail.com>
2018-05-07 22:07:16 -0300

    Renombrar variable

--- TGC.Group/Model/GameObjects/Escenario/Escenario1.cs
+++ TGC.Group/Model/GameObjects/Escenario/Escenario1.cs
@@ -19,7 +19,7 @@ namespace TGC.Group.Model.GameObjects
         // El piso del mapa/escenario
         private TgcPlane Piso;
         private List<TgcMesh> ListaPozos = new List<TgcMesh>();
-        private List<TgcMesh> ListaPlataformasGiratorias = new List<TgcMesh>();
+        private List<TgcMesh> ListaPlataformas = new List<TgcMesh>();
         private List<TgcMesh> ListaPisosResbalosos = new List<TgcMesh>();
         private List<TgcMesh> ListaMeshesSinColision = new List<TgcMesh>();
 
@@ -70,7 +70,7 @@ namespace TGC.Group.Model.GameObjects
             Plataformas.Add(new PlataformaGiratoria(20, Plataformas[0].Mesh.clone("pGira"), new TGCVector3(260f, 0f, 275f), 5f));
             foreach (var plataforma in Plataformas)
             {
-                ListaPlataformasGiratorias.Add(plataforma.Mesh);
+                ListaPlataformas.Add(plataforma.Mesh);
             }
 
             KDTree = new GrillaRegular();
@@ -117,7 +117,7 @@ namespace TGC.Group.Model.GameObjects
                     {
                         Env.Personaje.SetTipoColisionActual(TiposColision.Pozo);
                     }
-                    else if(ListaPlataformasGiratorias.Contains(Mesh))
+                    else if(ListaPlataformas.Contains(Mesh))
                     {
                         var Plataforma = Plataformas.Find(p=> p.Mesh == Mesh);
                         Env.Personaje.SetTipoColisionActual(TiposColision.Caja);
