9772252177abb5f9b883b6f50312ee5b9adab189
Nicolas <abramlujan@gmail.com>
2018-05-29 18:32:27 -0300

    SpriteButton para el menu

--- TGC.Group/Model/GameObjects/Escenario/EscenarioMenu.cs
+++ TGC.Group/Model/GameObjects/Escenario/EscenarioMenu.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.DirectX.DirectInput;
+﻿using Microsoft.DirectX.Direct3D;
+using Microsoft.DirectX.DirectInput;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using TGC.Core.BoundingVolumes;
 using TGC.Core.Collision;
+using TGC.Core.Direct3D;
 using TGC.Core.Geometry;
 using TGC.Core.Input;
 using TGC.Core.Mathematica;
@@ -17,35 +19,113 @@ delegate void ClickDelegate();
 
 namespace TGC.Group.Model.GameObjects.Escenario
 {
+    interface Button
+    {
+        void Render(GameModel Env);
+        TgcBoundingAxisAlignBox AABB();
+        void Click();
+    }
+    class QuadButton : Button
+    {
+        string text;
+        TGCQuad quad;
+        TgcBoundingAxisAlignBox aabb;
+        int x, y;
+        ClickDelegate onClick;
+        public System.Drawing.Font font = new System.Drawing.Font("Arial", 24, FontStyle.Bold);
+
+        public QuadButton(TGCVector3 pos, float X, float Y, string text, ClickDelegate onClick, Color color, float sizeX = 15, float sizeY = 8)
+        {
+
+            quad = new TGC.Core.Geometry.TGCQuad();
+            quad.Center = new TGCVector3(pos.X + X, pos.Y - 50, pos.Z + Y);
+            quad.Size = new TGCVector2(sizeX, sizeY);
+            quad.Normal = new TGCVector3(0, 1, 0);
+            quad.Color = color;
+            quad.updateValues();
+            var s = quad.Size * 0.5f;
+            aabb = new TgcBoundingAxisAlignBox(quad.Center - new TGCVector3(s.X, 0, s.Y), quad.Center + new TGCVector3(s.X, 0, s.Y), quad.Center, new TGCVector3(1, 1, 1));
+            x = (int)(X * 650 / 20 + 650);
+            y= (int)(Y * 350 / 20 + 350);
+            this.onClick = onClick;
+            this.text = text;
+        }
+        public void Render(GameModel Env)
+        {
+            Env.DrawText.changeFont(font);
+            Env.DrawText.drawText(text, x, y, Color.White);
+            Env.DrawText.changeFont(TGC.Core.Text.TgcText2D.VERDANA_10);
+            quad.Render();
+        }
+        public void Click()
+        {
+            onClick();
+        }
+        public TgcBoundingAxisAlignBox AABB()
+        { return aabb; }
+    }
+    class SpriteButton : Button
+    {
+        TgcBoundingAxisAlignBox aabb;
+        int x, y;
+        ClickDelegate onClick;
+        Sprite sprite;
+        Texture textura;
+        TGCVector3 Center;
+        TGCVector2 Size;
+
+        public SpriteButton(TGCVector3 pos, float X, float Y, string filePath, ClickDelegate onClick, float sizeX = 15, float sizeY = 8)
+        {
+            var d3dDevice = D3DDevice.Instance.Device;
+            //TgcTexture textura;
+            sprite = new Sprite(d3dDevice);
+            
+            try
+            {
+                 textura = TextureLoader.FromFile(d3dDevice, filePath);
+            }
+            catch
+            {
+                throw new Exception(string.Format("Error at loading texture: {0}", filePath));
+            }
+            Center = new TGCVector3(pos.X + X, pos.Y - 50, pos.Z + Y);
+            Size = new TGCVector2(sizeX, sizeY);
+            var s = Size * 0.5f;
+            aabb = new TgcBoundingAxisAlignBox(Center - new TGCVector3(s.X, 0, s.Y), Center + new TGCVector3(s.X, 0, s.Y), Center, new TGCVector3(1, 1, 1)); ;
+            x = (int)(X * 650 / 40 + 675);
+            y = (int)(Y * 350 / 20 + 365);
+            Size = Size * 18;
+            x -= (int)(Size.X * 0.5);
+            y -= (int)(Size.Y * 0.5);
+            this.onClick = onClick;
+        }
+        public void Render(GameModel Env)
+        {
+            sprite.Begin(SpriteFlags.None);
+            sprite.Draw2D(textura, Rectangle.Empty, new SizeF(Size.X, Size.Y), new PointF(x,y),  Color.White);
+            sprite.End();
+        }
+        public void Click()
+        {
+            onClick();
+        }
+        public TgcBoundingAxisAlignBox AABB()
+        { return aabb; }
+    }
     class EscenarioMenu : Escenario1
     {
         public TGCVector3 pos;
-        public Dictionary<string, Tuple<TGCQuad, ClickDelegate, int, int, TgcBoundingAxisAlignBox>> buttons = 
-            new Dictionary<string, Tuple<TGCQuad, ClickDelegate, int, int, TgcBoundingAxisAlignBox>>();
+        public List<Button> buttons = new List<Button>();
         private TgcPickingRay pickingRay;
 
-        // Masomenos +/-80 en x y +/-40 en y
-        private void addButton(float X, float Y, string text, ClickDelegate onClick, Color color, float sizeX=15, float sizeY=8)
-        {
-            var q = new TGC.Core.Geometry.TGCQuad();
-            q.Center = new TGCVector3(pos.X+X, pos.Y - 50, pos.Z+Y);
-            q.Size = new TGCVector2(sizeX, sizeY);
-            q.Normal = new TGCVector3(0, 1, 0);
-            q.Color = color;
-            q.updateValues();
-            var s = q.Size * 0.5f;
-            var aabb = new TgcBoundingAxisAlignBox(q.Center - new TGCVector3(s.X, 0, s.Y), q.Center + new TGCVector3(s.X, 0, s.Y), q.Center, new TGCVector3(1, 1, 1));
-            buttons[text] = new Tuple<TGCQuad, ClickDelegate, int, int, TgcBoundingAxisAlignBox>(q, onClick, (int)(X*650/20 + 650), (int)(Y*350/20 + 350), aabb);
-        }
         public override void Init(GameModel _env)
         {
             base.Init(_env);
             Env.Camara = new Core.Camara.TgcCamera();
             pos = new TGCVector3(1200, 800, -300);
             Env.Camara.SetCamera(pos, new TGCVector3(pos.X, 0, pos.Z-1));
-            addButton(0,-15,"Start", ()=> { Env.CambiarEscenario(0); }, Color.YellowGreen);
-            //addButton(0, 0, "Options", () => { }, Color.Aqua);
-            addButton(0, 15, "Exit", () => { Environment.Exit(0);  }, Color.Red);
+            buttons.Add(new SpriteButton(pos, 0, -15, Env.MediaDir + "\\Menu\\BotonIniciarPartida.jpg", () => { Env.CambiarEscenario(0); }));
+            buttons.Add(new QuadButton(pos, 0, 15, "Exit", () => { Environment.Exit(0); }, Color.Red));
             pickingRay = new TgcPickingRay(Env.Input);
         }
         public override void Render()
@@ -55,14 +135,14 @@ namespace TGC.Group.Model.GameObjects.Escenario
                 //Actualizar Ray de colision en base a posicion del mouse
                 pickingRay.updateRay();
 
-                foreach (KeyValuePair<string, Tuple<TGCQuad, ClickDelegate, int, int, TgcBoundingAxisAlignBox>> button in buttons)
+                foreach (var button in buttons)
                 {
-                    var aabb = button.Value.Item5;
+                    var aabb = button.AABB();
                     TGCVector3 collisionPoint;
                     var selected = TgcCollisionUtils.intersectRayAABB(pickingRay.Ray, aabb, out collisionPoint);
                     if (selected)
                     {
-                        button.Value.Item2(); 
+                        button.Click(); 
                         break;
                     }
                 }
@@ -73,13 +153,10 @@ namespace TGC.Group.Model.GameObjects.Escenario
             {
                 plano.Render();
             }
-            Env.DrawText.changeFont(new Font("Arial", 24, FontStyle.Bold));
-            foreach (KeyValuePair<string, Tuple<TGCQuad, ClickDelegate, int, int, TgcBoundingAxisAlignBox>> button in buttons)
+            foreach (var button in buttons)
             {
-                button.Value.Item1.Render();
-                Env.DrawText.drawText(button.Key, button.Value.Item3, button.Value.Item4, Color.White);
+                button.Render(Env);
             }
-            Env.DrawText.changeFont(TGC.Core.Text.TgcText2D.VERDANA_10);
             postRender3D();
             render2D();
         }
